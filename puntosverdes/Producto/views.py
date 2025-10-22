9a067ce8e7994cf26e79a1d9e892fb6fc932cedd
mmarilao2025-pixel <mmarilao2025@alu.uct.cl>
2025-10-22 14:45:57 -0300

    add: nuevo archico urls.py

--- puntosverdes/Producto/views.py
+++ puntosverdes/Producto/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from urllib import request
 
-# Create your views here.
+def Producto(request):
+    return HttpResponse('<h1>Hola Mundo/</h1>')
+
+def crear_producto():
+    pass
