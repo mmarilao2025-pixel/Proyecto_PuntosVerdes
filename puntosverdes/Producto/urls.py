edd4a971a0d1528647756bbc0e7b75143be95c65
mmarilao2025-pixel <mmarilao2025@alu.uct.cl>
2025-11-03 18:12:25 -0300

    add: archivos html

--- puntosverdes/Producto/urls.py
+++ puntosverdes/Producto/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'Producto'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("obtener/", views.obtener_todo, name="obtener_Productos"),
+    path('',views.inicio, name='inicio'),
 ]
--- puntosverdes/Producto/views.py
+++ puntosverdes/Producto/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from urllib import request
+from .models import Producto
 
 def Producto(request):
     return HttpResponse('<h1>Hola Mundo/</h1>')
 
-def crear_producto():
-    pass
+def obtener_todo(request):
+    productos = []
+    context = {"nombre": "Pan",
+               "cantidad": 1
+               }
+    return render(request, 'productos/obtener_producto.html')
+
+def inicio(request):
+    return render(request, 'productos/inicio.html')
